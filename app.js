bf8d78b3aaf7980b5c94a8937fd624bab99a9497
john.huang <pheno_the_best@yahoo.com.tw>
2024-07-15 21:19:35 +0800

    express

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { RestClient } = require("@fugle/marketdata");
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const client = new RestClient({
+    apiKey:
+      "ZjdmYzdmOWItYjNhYy00OWU0LWE3OWEtNGQyYjg0MzFhZmU2IGI5ZWJkOWI1LTA2ZTUtNGE4NS1hYzU1LTQ1MjU1ZmFlODg4Yw==",
+  });
+  const stock = client.stock;
+
+  //stock.intraday.quote({ symbol: "2330" }).then((data) => console.log(data));
+  const stockData = await stock.intraday.quote({ symbol: "2330" });
+  res.send(stockData);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
